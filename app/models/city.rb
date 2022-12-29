860941e3584f633d5fb73b8f62236583b97b15ec
Tamara Dowis <tamara.dowis@gmail.com>
2022-12-29 16:35:17 -0600

    update city model and rspec to inc relationships

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,8 @@
 class City < ApplicationRecord
   validates :name, presence: true
   validates :country, presence: true
+
+  has_many :events, dependent: :destroy
+  has_many :user_cities, dependent: :destroy
+  has_many :users, through: :user_cities
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe City, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:country) }
   end
+  describe 'relationships' do
+    it { should have_many(:events) }
+    it { should have_many(:user_cities) }
+    it { should have_many(:users).through(:user_cities) }
+  end
 end
